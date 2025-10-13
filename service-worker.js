7b744b8b76d8e89a1be32764ec32263282afe99b
Kariuki Peter Waweru <157828224+KINGp590@users.noreply.github.com>
2025-10-13 11:02:01 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,22 @@
-self.addEventListener("install", event => {
-    console.log("[NumVerse] Service Worker installing...");
-    self.skipWaiting();
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
+  e.waitUntil(
+    caches.open('numverse-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener("activate", event => {
-    console.log("[NumVerse] Service Worker activated.");
-});
-
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
